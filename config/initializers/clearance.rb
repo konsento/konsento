dcb68c8e7b2417c727ed20724d85a76167e56d98
Guilherme Ruiz <guiiruiz@hotmail.com>
2015-12-09 08:05:19 -0200

    Run clearance view generator and style sign view

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,3 +1,19 @@
 Clearance.configure do |config|
-  config.mailer_sender = "reply@example.com"
+  config.allow_sign_up = false
+  # config.cookie_domain = 'konsento.com.br'
+  # config.cookie_expiration = lambda { |cookies| 1.year.from_now.utc }
+  # config.cookie_name = 'konsento_remember_token'
+  # config.cookie_path = '/'
+  # config.routes = false
+  # config.httponly = false
+  config.mailer_sender = 'no-reply@konsento.com'
+  # config.password_strategy = Clearance::PasswordStrategies::BCrypt
+  # config.redirect_url = '/'
+  # config.secure_cookie = false
+  # config.sign_in_guards = []
+  # config.user_model = User
+
+  Clearance::PasswordsController.layout 'pages'
+  Clearance::SessionsController.layout 'pages'
+  Clearance::UsersController.layout 'pages'
 end
