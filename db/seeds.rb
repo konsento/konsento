90d5bf69069690c43375f9395676685e99869ebf
Guilherme Ruiz <guiiruiz@hotmail.com>
2015-12-16 18:39:36 -0200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 ActiveRecord::Base.transaction do
-  user = User.create(username: "Ruiz", email: "email@email.com", password: "123456")
+  user1 = User.create(username: "ruiz", email: "ruiz@email.com", password: "123456")
+  user2 = User.create(username: "anon", email: "anon@email.com", password: "123456")
+  user3 = User.create(username: "tanaka", email: "tanaka@email.com", password: "123456")
 
   join = JoinRequirement.create(title: "CPF");
 
-  req =  RequirementValue.create(user: user, join_requirement: join, value: 12345678900)
+  req =  RequirementValue.create(user: user1, join_requirement: join, value: 12345678900)
 
   group = Group.create(parent: nil, title: "Global", description: "Global group.")
 
   subgroup = Group.create(parent: group, title: "National", description: "Global subgroup.")
 
 
-  topic = Topic.create(user: user, group: group, title: "Sample Topic")
+  topic = Topic.create(user: user1, group: group, title: "Sample Topic")
 end
