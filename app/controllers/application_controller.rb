7d797a89c6dacce27dde230889ec3c29249725e4
Anon <anon@anon.com>
2016-01-14 19:46:27 -0200

    Install the responders gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
+require "application_responder"
+
 class ApplicationController < ActionController::Base
+  include Clearance::Controller
+
+  self.responder = ApplicationResponder
+  respond_to :html
+
   before_filter :add_root_breadcrumb
 
-  include Clearance::Controller
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
