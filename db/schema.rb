cce257db1b180e280f624de26e193a5b646ba002
Anon <anon@anon.com>
2016-01-18 23:57:04 -0200

    Remove pg_search_documents table since it's not needed for scoped searches

--- db/schema.rb
+++ db/schema.rb
@@ -65,16 +65,6 @@ ActiveRecord::Schema.define(version: 20160119004732) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "pg_search_documents", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "searchable_id"
-    t.string   "searchable_type"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
-  add_index "pg_search_documents", ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id", using: :btree
-
   create_table "proposals", force: :cascade do |t|
     t.integer  "user_id",    null: false
     t.integer  "parent_id"
