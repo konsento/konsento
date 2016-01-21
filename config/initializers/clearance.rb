868c0eaa25f870e0404ff3c9a3076b0163b1f91e
Anon <anon@anon.com>
2016-01-21 04:03:07 -0200

    Fix clearance mail sender

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,12 +1,12 @@
 Clearance.configure do |config|
   config.allow_sign_up = true
-  # config.cookie_domain = 'konsento.com.br'
+  # config.cookie_domain = 'konsento.org'
   # config.cookie_expiration = lambda { |cookies| 1.year.from_now.utc }
   # config.cookie_name = 'konsento_remember_token'
   # config.cookie_path = '/'
   config.routes = false
   # config.httponly = false
-  config.mailer_sender = 'no-reply@konsento.com'
+  config.mailer_sender = 'noreply@konsento.org'
   # config.password_strategy = Clearance::PasswordStrategies::BCrypt
   # config.redirect_url = '/'
   # config.secure_cookie = false
