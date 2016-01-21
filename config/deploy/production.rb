ae94a9ff9e011496d1e226c2f1dd1ee1bc0606b0
Anon <anon@anon.com>
2016-01-21 04:07:01 -0200

    Update capistrano production domains

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :stage, :production
 
-role :app, "deploy@ec2-52-32-213-43.us-west-2.compute.amazonaws.com"
-role :web, "deploy@ec2-52-32-213-43.us-west-2.compute.amazonaws.com"
-role :db,  "deploy@ec2-52-32-213-43.us-west-2.compute.amazonaws.com"
+role :app, "deploy@konsento.org"
+role :web, "deploy@konsento.org"
+role :db,  "deploy@konsento.org"
