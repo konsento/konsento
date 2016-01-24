4432afef8655eeb2b0102bd1d9eca2b39afeac22
Guilherme Ruiz <guiiruiz@hotmail.com>
2016-01-24 14:34:54 -0200

    Set I18n locale by users browser language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
   respond_to :html
 
   before_filter :add_root_breadcrumb
+  before_filter :set_locale
   before_action :set_js_data
 
   # Prevent CSRF attacks by raising an exception.
@@ -25,5 +26,24 @@ class ApplicationController < ActionController::Base
       action: params[:action].classify,
       params: params
     )
+  end
+  def set_locale
+    locale = :en
+    browser_locale = get_browser_lang.to_s
+    if browser_locale == 'pt'
+      browser_locale = 'pt-BR'
+    end
+    if I18n.available_locales.map(&:to_s).include? browser_locale
+      locale = browser_locale
+    end
+    @curlang = I18n.locale = locale.to_s
+  end
+
+  def get_browser_lang
+    if request.env['HTTP_ACCEPT_LANGUAGE'].to_s.length > 0
+      request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    else
+      'en'
+    end
   end
 end
