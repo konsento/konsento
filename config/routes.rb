aca774470337c4ba315dbcf9851db8ea7dc834fc
Guilherme Ruiz <guiiruiz@hotmail.com>
2016-01-26 15:17:56 -0200

    Create teams controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
     get '/search_proposals' => 'topics#search_proposals', on: :member
   end
 
+  resources :teams, only: [:show]
   resources :proposals, only: [:create]
   resources :subscriptions, only: [:create, :destroy]
   resources :comments, only: [:create]
