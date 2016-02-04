51d14e690b9f0f04d0d599331ed5c4c3912851d3
Guilherme Ruiz <guiiruiz@hotmail.com>
2016-02-04 16:53:32 -0200

    Implement CommentObserver to create notifications

---
+++ app/models/comment_observer.rb
@@ -0,0 +1,28 @@
+class CommentObserver < ActiveRecord::Observer
+  def after_create(comment)
+    case comment.commentable_type
+    when 'Topic'
+      key = 'new_topic_comment'
+      data = {
+        'topic_title' => comment.commentable.title
+      }
+    when 'Proposal'
+      key = 'new_proposal_comment'
+      data = {
+        'topic_title' => comment.commentable.topic.title
+      }
+    end
+
+    users = comment.commentable.comments.group(:user_id).pluck(:user_id)
+    users.delete(comment.user_id)
+
+    users.each do |user_id|
+      Notification.create(
+        user_id: user_id,
+        key: key,
+        data: data,
+        notifiable: comment
+      )
+    end
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,6 @@ module Konsento
     
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.active_record.observers = :comment_observer
   end
 end
