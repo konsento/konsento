6a622a96ae2156409e0d64b4fbc81ddb1c14af44
Guilherme Ruiz <guiiruiz@hotmail.com>
2016-02-05 00:44:43 -0200

    Add read/unread scopes to Notification model

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,7 @@
 class Notification < ActiveRecord::Base
   belongs_to :user
   belongs_to :notifiable, polymorphic: true, touch: true
+
+  scope :read, -> { where(read: true) }
+  scope :unread, -> { where(read: false) }
 end
