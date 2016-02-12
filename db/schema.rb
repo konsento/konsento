0ef9ada4bef55f196f390fa45de915325613693c
Guilherme Ruiz <guiiruiz@hotmail.com>
2016-02-12 19:11:47 -0200

    Update schema.rb to fix branch merge bug

--- db/schema.rb
+++ db/schema.rb
@@ -116,7 +116,7 @@ ActiveRecord::Schema.define(version: 20160212202640) do
 
   create_table "sections", force: :cascade do |t|
     t.integer  "topic_id",   null: false
-    t.integer  "index",      null: false
+    t.integer  "index"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
