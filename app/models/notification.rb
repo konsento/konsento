67474159d4fcc3c437b4c25102a79db225396118
Guilherme Ruiz <guiiruiz@hotmail.com>
2016-02-16 01:46:55 -0200

    Use notification model as notification center and refactor observers code

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,4 +4,67 @@ class Notification < ActiveRecord::Base
 
   scope :read, -> { where(read: true) }
   scope :unread, -> { where(read: false) }
+
+  def self.notify(notifiable)
+    self.send("notify_#{notifiable.model_name.element}", notifiable)
+  end
+
+  private
+  def self.notify_comment(comment)
+    case comment.commentable_type
+    when 'Topic'
+      key = 'new_topic_comment'
+      data = {
+        'topic_title' => comment.commentable.title
+      }
+    when 'Proposal'
+      key = 'new_proposal_comment'
+      data = {
+        'topic_title' => comment.commentable.topic.title
+      }
+    end
+
+    users = comment.commentable.comments.group(:user_id).pluck(:user_id)
+    users << comment.commentable.user_id
+    users.delete(comment.user_id)
+
+    users.each do |user_id|
+      Notification.create(
+        user_id: user_id,
+        key: key,
+        data: data,
+        notifiable: comment
+      )
+    end
+  end
+
+  def self.notify_team_invitation(team_invitation)
+    key = 'team_invitation'
+    data = { 'team_title' => team_invitation.team.title }
+    user = User.find_by(email: team_invitation.email)
+    if user
+      Notification.create(
+        user_id: user.id,
+        key: key,
+        data: data,
+        notifiable: team_invitation
+      )
+    end
+  end
+
+  def self.notify_topic(topic)
+    key = 'new_topic'
+    data = { 'group_title' => topic.group.title }
+    users = topic.group.users.pluck(:id)
+    users.delete(topic.user_id)
+
+    users.each do |user_id|
+      Notification.create(
+        user_id: user_id,
+        key: key,
+        data: data,
+        notifiable: topic
+      )
+    end
+  end
 end
--- app/models/team_invitation_observer.rb
+++ app/models/team_invitation_observer.rb
@@ -1,15 +1,5 @@
 class TeamInvitationObserver < ActiveRecord::Observer
   def after_create(team_invitation)
-    key = 'team_invitation'
-    data = { 'team_title' => team_invitation.team.title }
-    user = User.find_by(email: team_invitation.email)
-    if user
-      Notification.create(
-        user_id: user.id,
-        key: key,
-        data: data,
-        notifiable: team_invitation
-      )
-    end
+    Notification.notify(team_invitation)
   end
 end
