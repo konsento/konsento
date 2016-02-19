0881283f7dd9cb3d9f93005cb2ce34ba9723fbfe
Anon <anon@anon.com>
2016-02-19 01:19:52 -0200

    Fix cookies TLD length

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_konsento_session', domain: :all, tld_length: 3
+Rails.application.config.session_store :cookie_store, key: '_konsento_session', domain: :all, tld_length: 1
