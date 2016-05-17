874347e9e2e8da80f3101486dc98bc02823a91ba
Guilherme Ruiz <guiiruiz@hotmail.com>
2016-05-16 23:42:54 -0300

    Fix tests

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -6,6 +6,6 @@ RSpec.describe Notification, type: :model do
   it { is_expected.to validate_presence_of :key }
   it { is_expected.to validate_presence_of :data }
   it { is_expected.to validate_presence_of :notifiable }
-  it { is_expected.to validate_presence_of :read }
+  it { is_expected.to validate_inclusion_of(:read).in_array([true, false]) }
   it { is_expected.to belong_to :user }
 end
--- spec/models/team_invitation_spec.rb
+++ spec/models/team_invitation_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe TeamInvitation, type: :model do
   subject { build :team_invitation }
   it { is_expected.to validate_presence_of :team }
   it { is_expected.to validate_presence_of :token }
-  it { is_expected.to validate_presence_of :accepted }
+  it { is_expected.to validate_inclusion_of(:accepted).in_array([true, false]) }
   it { is_expected.to belong_to :team }
 
 end
