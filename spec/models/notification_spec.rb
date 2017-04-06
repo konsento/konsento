69417e090f7de622605f6932c2a2398f8d6787f0
Guilherme Ruiz <guiiruiz@hotmail.com>
2017-04-06 01:04:04 -0300

    Remove validate_presence_of data on Notification spec (136f13e28d676d377604e7c51ba4004a329f87da)

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Notification, type: :model do
   subject { build :notification }
   it { is_expected.to validate_presence_of :user }
   it { is_expected.to validate_presence_of :key }
-  it { is_expected.to validate_presence_of :data }
   it { is_expected.to validate_presence_of :notifiable }
   it { is_expected.to validate_inclusion_of(:read).in_array([true, false]) }
   it { is_expected.to belong_to :user }
