db38cf9595a31b59f61014ace0b126ff116f4d92
Anon <anon@anon.com>
2017-05-23 01:12:35 -0300

    Make bulgarian an available locale

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Konsento
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.i18n.available_locales = [:'pt-BR', :en, :es]
+    config.i18n.available_locales = [:'pt-BR', :en, :es, :bg]
     config.i18n.default_locale = :'pt-BR'
 
     # ActiveJob
